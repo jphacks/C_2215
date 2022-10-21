054214e97b8a149356dbedd4cb42cba57ac2344b
takuhiro uzawa <takuhirouzawa@MacBookPro16UT.lan>
2022-10-22 01:19:34 +0900

    first commit

--- docker/front.Dockerfile
+++ docker/front.Dockerfile
@@ -1,12 +1,13 @@
 FROM node:16.13.0
 # USER node
-WORKDIR /front/react-project
+WORKDIR /front/shop-front
 
 RUN npm install --save prop-types
 RUN npm install -g create-react-app
-# RUN npm install react react-dom
-# RUN npm install react-scripts
-# RUN npm install create-react-app
+RUN npm install react react-dom
+RUN npm install --save styled-components
+RUN npm install react-scripts
+RUN npm install create-react-app
 # RUN npm install @mui/material @emotion/react @emotion/styled
 # RUN npm install react-router-dom react-router
 # RUN npm install react-hook-form
---
+++ front/shop-front/src/Meter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Meter.css';
+
+class Meter extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    
+    render() {
+        return(
+            <div id="pie-chart" class="content">
+                <div class="pie-chart-wrap">
+                <div class="box">
+                    <div class="percent">
+                    <svg>
+                        <circle class="base" cx="75" cy="75" r="70"></circle>
+                        <circle class="line" cx="75" cy="75" r="70"></circle>
+                    </svg>
+                    <div class="number">
+                        <h3 class="title">{this.props.percent}<span>%</span></h3>
+                    </div>
+                    <p class="text">Graph {this.props.id}</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Meter;
